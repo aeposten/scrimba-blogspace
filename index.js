53de6e1ebc6b5a21339b18cb1f3cc94fc8c54e68
Amy <86963506+aeposten@users.noreply.github.com>
2023-01-09 10:11:00 -0800

    Challenge 3

--- index.js
+++ index.js
@@ -9,6 +9,27 @@
  * Log the response data to the console
  */
 
-fetch("https://apis.scrimba.com/jsonplaceholder/posts")
-  .then((response) => response.json())
-  .then((data) => console.log(data.slice(0, 5)));
+const allPostsDiv = document.getElementById("all-posts");
+
+function fetchPosts() {
+  fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+    .then((response) => response.json())
+    .then((data) => renderPosts(data.slice(0, 5)));
+}
+
+function renderPosts(postArray) {
+  const postHTML = postArray
+    .map(
+      (post) => `
+    <article class="post">
+        <h2 class="post-title">${post.title}</h2>
+        <div class="post-body">${post.body}</div>
+    </article>
+`
+    )
+    .join("");
+
+  allPostsDiv.innerHTML = postHTML;
+}
+
+fetchPosts();
