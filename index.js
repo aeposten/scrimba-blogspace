ed98d00b5bc0b0e61a878178c08304c6cd229404
Amy <86963506+aeposten@users.noreply.github.com>
2023-01-09 09:34:47 -0800

    Challenge 2

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 /**
  * Challenge:
- * 
+ *
  * Send a request to the JSON Placeholder API using `fetch`
  * URL: https://apis.scrimba.com/jsonplaceholder/posts
- * 
+ *
  * Documentation: https://jsonplaceholder.typicode.com/
- * 
+ *
  * Log the response data to the console
  */
 
- fetch('https://apis.scrimba.com/jsonplaceholder/posts')
- .then((response) => response.json())
- .then((data) => console.log(data))
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+  .then((response) => response.json())
+  .then((data) => console.log(data.slice(0, 5)));
